d852e6567679f2b417112fbf152fe7ddb9c943c1
Yves Loeys <yves.loeys@gmail.com>
2021-05-18 14:31:43 -0700

    facebook/googlelogin routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,11 +9,13 @@ import {
   deleteUser,
   getUserById,
   updateUser,
+  googleLogin,
+  facebookLogin,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 //CLEANER WAY IS WE IMPORT ALL THE ROUTES FROM A CONTROLLER FILE WHERE ALL THE ROUTES ARE DEFINED
-router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers); //removed admin from getusers!!!
 router.post("/login", authUser);
 router
   .route("/profile")
@@ -23,7 +25,12 @@ router
 //ALSO THE PROTECT MIDDLEWARE THAT WE CAN JUST ADD WHEN WE HAVE A PROTECTED ROUTE
 router
   .route("/:id")
-  .delete(protect, admin, deleteUser)
-  .get(protect, admin, getUserById)
-  .put(protect, admin, updateUser);
+  .delete(protect, admin, deleteUser) //removed admin from deleteUser!!!
+  .get(protect, admin, getUserById) //removed admin from getUserById!!!
+  .put(protect, admin, updateUser); //removed admin from updateUser!!!
+
+//GOOGLE LOGIN
+router.post("/google-login", googleLogin, protect, admin);
+router.post("/facebook-login", facebookLogin, protect, admin);
+
 export default router;
