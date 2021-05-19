0f60fc18f04fbd4236df429a59d64a5d1248bbb9
Yves Loeys <yves.loeys@gmail.com>
2021-05-19 08:22:08 -0700

    conditional landingpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -23,51 +23,64 @@ import ContactScreen from "./screens/ContactScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import EditProductScreen from "./screens/EditProductScreen";
 import OrderListScreen from "./screens/OrderListScreen";
-import Dashboard from "./screens/Dashboard";
+import DashboardScreen from "./screens/DashboardScreen";
+import Landing from "./screens/Landing";
 
 const App = () => {
+  const [show, setShow] = useState(true);
+
+  const showHomeHandler = () => {
+    setShow(true);
+  };
+
   return (
     <Router>
-      <main
-        className="py-3"
-        style={{
-          backgroundImage: `url(${Background})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <Header />
+      {show ? (
+        <main
+          className="py-3"
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <Header />
 
-        <Container>
-          <Route path="/shipping" component={Shippingscreen} />
-          <Route path="/payment" component={Paymentscreen} />
-          <Route path="/placeorder" component={PlaceOrderscreen} />
-          <Route path="/order/:id" component={Orderscreen} />
-          <Route path="/admin/orderlist" component={OrderListScreen} />
-          <Route path="/login" component={Loginscreen} />
-          <Route path="/register" component={Registerscreen} />
-          <Route path="/profile" component={Profilescreen} />
-          <Route path="/product/:id" component={Productscreen} />
-          <Route path="/admin/productlist" component={ProductListScreen} />
-          <Route path="/admin/dashboard" component={Dashboard} />
-          <Route path="/admin/product/:id/edit" component={EditProductScreen} />
-          <Route path="/cart/:id?" component={Cartscreen} />
-          <Route path="/contacts" component={ContactScreen} />
-          <Route path="/admin/userlist" component={UserListscreen} />
-          <Route path="/admin/user/:id/edit" component={UserEditscreen} />
-          <Route path="/fruity" component={FruityProductsScreen} />
-          <Route path="/casual" component={CasualProductsScreen} />
-          <Route path="/luxury" component={LuxuryProductsScreen} />
-          <Route
-            path="/admin/productlist/search/:keyword"
-            component={ProductListScreen}
-          />
-          <Route path="/" component={Homescreen} exact />
-        </Container>
-      </main>
-
-      <Footer />
+          <Container fluid style={{ maxWidth: "75%" }}>
+            <Route path="/shipping" component={Shippingscreen} />
+            <Route path="/payment" component={Paymentscreen} />
+            <Route path="/placeorder" component={PlaceOrderscreen} />
+            <Route path="/order/:id" component={Orderscreen} />
+            <Route path="/admin/orderlist" component={OrderListScreen} />
+            <Route path="/login" component={Loginscreen} />
+            <Route path="/register" component={Registerscreen} />
+            <Route path="/profile" component={Profilescreen} />
+            <Route path="/product/:id" component={Productscreen} />
+            <Route path="/admin/productlist" component={ProductListScreen} />
+            <Route path="/admin/dashboard" component={DashboardScreen} />
+            <Route
+              path="/admin/product/:id/edit"
+              component={EditProductScreen}
+            />
+            <Route path="/cart/:id?" component={Cartscreen} />
+            <Route path="/contacts" component={ContactScreen} />
+            <Route path="/admin/userlist" component={UserListscreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditscreen} />
+            <Route path="/fruity" component={FruityProductsScreen} />
+            <Route path="/casual" component={CasualProductsScreen} />
+            <Route path="/luxury" component={LuxuryProductsScreen} />
+            <Route
+              path="/admin/productlist/search/:keyword"
+              component={ProductListScreen}
+            />
+            <Route path="/" component={Homescreen} exact />
+          </Container>
+          <Footer />
+        </main>
+      ) : (
+        <Landing enterHomePage={showHomeHandler} />
+      )}
     </Router>
   );
 };
