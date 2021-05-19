8a8559b17453a92886465eabf167b552ffd02e8e
Yves Loeys <yves.loeys@gmail.com>
2021-05-19 08:05:30 -0700

    cleanup

--- frontend/src/screens/PlaceOrderscreen.js
+++ frontend/src/screens/PlaceOrderscreen.js
@@ -1,45 +1,42 @@
 import React, { useEffect } from "react";
 import { Button, Row, Col, Image, Card, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
-//COMPONENTS
 import Notification from "../components/Notification";
 import CheckoutSteps from "../components/CheckoutSteps";
-//REDUX
 import { useDispatch, useSelector } from "react-redux";
 import { createOrder } from "../redux/actions/orderActions";
 import { ORDER_CREATE_RESET } from "../redux/types";
 import { USER_DETAILS_RESET } from "../redux/types";
 
 const PlaceOrderscreen = ({ history }) => {
-  //redux..
-  const dispatch = useDispatch();
-  //Grabbing the items from the cart state with useSelector
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-  //Optional , function to always have 2 numbers behind the comma
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
   const addDecimals = (num) => {
     return (Math.round(num * 100) / 100).toFixed(2);
   };
+
   //Calculate \Total Price Item before tax/shipping
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
 
   //Calculate \Shipping
-  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
+  const shipping = addDecimals(Number((0.1 * cart.itemsPrice).toFixed(2)));
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 80 ? 0 : shipping);
 
   //Calculate \Tax
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
 
   //Calculate \Total Price
-  cart.totalPrice =
+  cart.totalPrice = (
     Number(cart.itemsPrice) +
     Number(cart.shippingPrice) +
-    Number(cart.taxPrice);
-
-  //Grabbing the orderCreate from the state and destructure order,success and error
-  const orderCreate = useSelector((state) => state.orderCreate);
-  const { order, success, error } = orderCreate;
+    Number(cart.taxPrice)
+  ).toFixed(2);
 
   useEffect(() => {
     if (success) {
@@ -47,10 +44,10 @@ const PlaceOrderscreen = ({ history }) => {
       dispatch({ type: USER_DETAILS_RESET });
       dispatch({ type: ORDER_CREATE_RESET });
     }
-  }, [dispatch, history, success, order._id]);
+    // eslint-disable-next-line
+  }, [dispatch, history, success]);
 
   const placeOrderHandler = () => {
-    //dispatching our redux action
     dispatch(
       createOrder({
         orderItems: cart.cartItems,
@@ -88,7 +85,7 @@ const PlaceOrderscreen = ({ history }) => {
             <ListGroup.Item>
               <h2>Order Items</h2>
               {cart.cartItems.length === 0 ? (
-                <Notification>Yout cart is empty</Notification>
+                <Notification>Your cart is empty</Notification>
               ) : (
                 <ListGroup variant="flush">
                   {cart.cartItems.map((item, index) => (
