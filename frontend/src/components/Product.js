52b3815e4a374be45b0560b7ca3c4e2e98858aa6
Yves Loeys <yves.loeys@gmail.com>
2021-05-18 18:27:55 -0700

    style update

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-//import Rating from "./Rating";
+import { useHistory } from "react-router-dom";
 
 const Product = ({ product }) => {
   let history = useHistory();
 
   const clickedBtn = (event) => {
-    //console.log(event.currentTarget.dataset.id);
     if (event.currentTarget.dataset.id === "1") {
       history.push("/fruity");
     } else if (event.currentTarget.dataset.id === "2") {
@@ -17,25 +15,20 @@ const Product = ({ product }) => {
     }
   };
 
-  const bg = { backgroundColor: "rgba(245, 245, 245, 0.3)" };
-
   return (
-    <Card className="my-3 p-3 rounded shadow-sm p-3 mb-5  rounded" style={bg}>
-      <Card.Img
-        src={product.image}
-        variant="top"
-        data-id={product.cat}
-        onClick={clickedBtn}
-        height="auto"
-        width="100%"
-      />
+    <Card
+      className="my-3 p-3 rounded shadow-sm p-3 mb-5  rounded"
+      style={{ backgroundColor: "rgba(245, 245, 245, 0.3)" }}
+      data-id={product.cat}
+      onClick={clickedBtn}
+    >
+      <Card.Img src={product.image} variant="top" height="auto" width="100%" />
 
       <Card.Body>
-        <Link to={"/allproducts"}>
-          <Card.Title as="div">
-            <h2>{product.name}</h2>{" "}
-          </Card.Title>
-        </Link>
+        <Card.Title as="div">
+          <h2>{product.name}</h2>{" "}
+        </Card.Title>
+
         <Card.Text as="h6" className="text-center text-secondary">
           Starts at ${product.price}
         </Card.Text>
