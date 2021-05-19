5b6c59f9fa800a69dbae91962ca0c0ff15d3aea4
Yves Loeys <yves.loeys@gmail.com>
2021-05-18 18:11:34 -0700

    update model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -14,6 +14,8 @@ const orderSchema = mongoose.Schema(
         qty: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
+        category: { type: String, required: true },
+
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
